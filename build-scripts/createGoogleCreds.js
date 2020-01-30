a1fea3fb4d9bf5a0359112d7ccceeabb5e8a096e
George Rodier <grodier3@gmail.com>
2020-01-29 22:25:40 -0500

    Correctly parse correct key

--- build-scripts/createGoogleCreds.js
+++ build-scripts/createGoogleCreds.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const creds = {
   type: 'service_account',
   project_id: process.env.FY_FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FY_FIREBASE_PRIVATE_KEY_ID.replace(/\\n/g, '\n'),
-  private_key: process.env.FY_FIREBASE_PRIVATE_KEY,
+  private_key_id: process.env.FY_FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FY_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.FY_FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FY_FOREBASE_CLIENT_ID,
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
