dd2b1a02a249d2a74e31781eaa6eb85967cce579
George Rodier <grodier3@gmail.com>
2020-02-05 21:01:03 -0500

    Remove unneeded form and api's

--- pages/api/testPost.js
+++
@@ -1,5 +0,0 @@
-function testPost(req, res) {
-  res.status(200).json({ name: req.body });
-}
-
-export default testPost;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 
-function GreetingPhrase(name) {
-  return name ? `Hello, ${name}!` : `Hello, World!`;
-}
-
-async function postComment(data) {
-  return await fetch('/api/testPost', {
-    method: 'POST', // or 'PUT'
-    body: JSON.stringify(data), // data can be `string` or {object}!
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-}
-
 function Home() {
-  const [name, setName] = useState();
-  const [inputVal, setInputVal] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('/api/testFunction', {
-        method: 'GET',
-      });
-      const json = await response.json();
-      setName(json.name);
-    };
-    fetchData();
-  }, []);
-
-  async function handleSubmit(event) {
-    alert('A name was submitted: ' + inputVal);
-    event.preventDefault();
-    const response = await postComment(inputVal);
-    const json = await response.json();
-    setName(json.name);
-  }
-
-  function handleChange(event) {
-    setInputVal(event.target.value);
-  }
-
   return (
     <>
       <Link href="/signin">
@@ -50,14 +10,6 @@ function Home() {
       <Link href="/signup">
         <a>Try it out now!</a>
       </Link>
-      {GreetingPhrase(name)}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={inputVal} onChange={handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
     </>
   );
 }
